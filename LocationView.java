984d33f6c461f6ec8eadb9257ea04d4c69dfb710
Josh Clemens <clemensjosh@protonmail.com>
2019-04-15 20:32:50 -0400

    added unimplemented methods so LocationView would compile

--- LocationView.java
+++ LocationView.java
@@ -1,2 +1,14 @@
+import java.awt.Graphics2D;
+
 public class LocationView extends GameObject {
+
+    @Override
+    public void update(GameObject o) {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
 } // end LocationView 
