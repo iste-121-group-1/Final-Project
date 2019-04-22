3d27072bfb5e6634078c7c768cc67c50666f08ae
Josh Clemens <clemensjosh@protonmail.com>
2019-04-22 18:25:00 -0400

    add update methods, update documentation

--- finalGame/GameData.java
+++ finalGame/GameData.java
@@ -21,12 +21,32 @@ public class GameData extends DataObject {
     public int xpos;
     public int ypos;
 
+    /**
+     * GameData consturctor
+     * @param _state the GAME_STATES passed from the client
+     * @param _name the client's username
+     * @param _color the client player's color
+     * @param _xpos player x position
+     * @param _ypos player y position
+     */
     public GameData(GAME_STATES _state, String _name, Color _color, int _xpos, int _ypos) {
         type = GAME;
         state = _state;
         name = _name;
         color = _color;
         xpos = _xpos;
+        ypos = _ypos;
+    }
+
+    public void updateGameState(GAME_STATES _state) {
+        state = _state;
+    }
+
+    public void updateXpos(int _xpos) {
+        xpos = _xpos;
+    }
+
+    public void updateYpos(int _ypos) {
         ypos = _ypos;
     }
 }
