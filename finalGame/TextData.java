f4d44433d8a233750214dd9de555122b524911aa
Josh Clemens <clemensjosh@protonmail.com>
2019-04-24 09:22:06 -0400

    updated TextData constructors and documentation

--- finalGame/TextData.java
+++ finalGame/TextData.java
@@ -9,14 +9,33 @@ public class TextData extends DataObject {
 
     public DataType type;
     public String message;
+    public String username;
 
+    /**
+     * Constructor for creating an empty message
+     */
     public TextData() {
         type = TEXT;
         message = "";
     }
     
+    /**
+     * Constructor for sending a message tied to no player to all clients
+     * @param _message the message being sent
+     */
     public TextData(String _message) {
         type = TEXT;
+        message = _message;
+    }
+
+    /**
+     * Constructor for sending a message from a specific user to all clients
+     * @param _username the user the message is being sent from
+     * @param _message the message being sent
+     */
+    public TextData(String _username, String _message) {
+        type = TEXT;
+        username = _username;
         message = _message;
     }
 }
