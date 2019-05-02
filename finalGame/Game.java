6dc15ed8437104cf3fc456a28856c6b9330dfaaa
winterbreezesrule <48916147+winterbreezesrule@users.noreply.github.com>
2019-05-02 14:32:55 -0400

    WhoIsIn

--- finalGame/Game.java
+++ finalGame/Game.java
@@ -181,7 +181,7 @@ public class Game extends JFrame implements KeyListener {
       });
 
       about.addActionListener(ae -> {
-         JOptionPane.showMessageDialog(null, "You can write something :)");
+         JOptionPane.showMessageDialog(null, "Cool Platformer by Cool Team.");
       });
 
       // Create panels for the frame
@@ -301,9 +301,11 @@ public class Game extends JFrame implements KeyListener {
          }
       });
       jbWhoIsIn.addActionListener(ae -> {
+        String playerlist = "Connected: ";
          for (GameData player : connectedPlayers) {
-            JOptionPane.showMessageDialog(null, player.getName());
+            playerlist += player.getName() + " ";
          }
+         JOptionPane.showMessageDialog(null, playerlist);
       });
       jbColor.addActionListener(colorChooser);
 
@@ -332,9 +334,6 @@ public class Game extends JFrame implements KeyListener {
       jpNorth.add(scroll);
       add(jpNorth, BorderLayout.CENTER);
 
-      // menu.setVisible(true);
-      // jp.setVisible(true);
-      // this.getContentPane().add(menu, BorderLayout.CENTER);
       // END MENU/CHAT UI //
 
       // LEADERBOARD UI //
