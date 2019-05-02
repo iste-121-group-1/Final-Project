f00df68441e1186dbb53a4beae9235e53830abb8
winterbreezesrule <48916147+winterbreezesrule@users.noreply.github.com>
2019-05-02 12:45:55 -0400

    Camera movement is working. Now for fixing collision.

--- finalGame/Terrain.java
+++ finalGame/Terrain.java
@@ -7,15 +7,16 @@ public class Terrain extends GameObject {
     private ArrayList<TerrainComponents> myGround;
     private ArrayList<Rectangle> myRects;
     private int finishLine;
+    private boolean offsetCamera = true;
     
     public Terrain() {
         myRects = new ArrayList<Rectangle>();
         myGround = new ArrayList<TerrainComponents>();
-        myGround.add(new TerrainComponents(0, 620, 4000, 100));
+        myGround.add(new TerrainComponents(0, 620, 4050, 100));
         myGround.add(new TerrainComponents(500, 420, 50, 50));
         myGround.add(new TerrainComponents(750, 420, 250, 50));
         myGround.add(new TerrainComponents(850, 270, 50, 50));
-        /*myGround.add(new TerrainComponents(1100, 520, 100, 100));
+        myGround.add(new TerrainComponents(1100, 520, 100, 100));
         myGround.add(new TerrainComponents(1350, 470, 100, 150));
         myGround.add(new TerrainComponents(1600, 420, 100, 200));
         myGround.add(new TerrainComponents(2400, 420, 200, 50));
@@ -28,7 +29,7 @@ public class Terrain extends GameObject {
         myGround.add(new TerrainComponents(3600, 320, 200, 50));
         myGround.add(new TerrainComponents(3650, 270, 150, 50));
         myGround.add(new TerrainComponents(3700, 220, 100, 50));
-        myGround.add(new TerrainComponents(3750, 170, 50, 50));*/
+        myGround.add(new TerrainComponents(3750, 170, 50, 50));
     } // end Terrain constructor
     
     public void update(GameObject o) {
@@ -41,22 +42,29 @@ public class Terrain extends GameObject {
         for (TerrainComponents ground : myGround) {
             ground.draw(g);
         }
-        
-        g.setColor(Color.RED);
-        g.drawLine(3950, 0, 3950, 620);
+        if (!offsetCamera) {
+            g.setColor(Color.BLUE);
+            g.drawLine(1050, 0, 1050, 620);
+        }
     }
     
     public ArrayList<Rectangle> getTerrain() {
         return myRects;
     } // end getTerrain
     
-    public void offsetCamera(int offsetPos) {
-        for (TerrainComponents comp : myGround) {
-            comp.setX(comp.getX() - offsetPos);
+    public boolean offsetCamera(int offsetPos, int disTravelled) {
+        if (offsetCamera) {
+            for (TerrainComponents comp : myGround) {
+                comp.setX(comp.getX() - offsetPos);
+            }
+            for (Rectangle rect : myRects) {
+                rect.setLocation((int) rect.getX() - offsetPos, (int) rect.getY());
+            }
+            if (disTravelled >= 2920) {
+                offsetCamera = false;
+            }
         }
-        for (Rectangle rect : myRects) {
-            rect.setLocation((int) rect.getX() - offsetPos, (int) rect.getY());
-        }
+        return offsetCamera;
     } // end offsetCamera()
     
     class TerrainComponents extends GameObject {
